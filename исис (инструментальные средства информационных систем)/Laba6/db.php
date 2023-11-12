f81c52a19bc13abb3ac053ffcab8cd34d249fa2c
HP <titarenkomiroslav61@gmail.com>
2023-11-12 23:20:56 +0300

    кароче я хз че с 6 лабой по исису делать просто не идет с самого начала, наверно отправлю письмо Марии Валерьевне о том что не получается(((

--- исис (инструментальные средства информационных систем)/Laba6/db.php
+++ исис (инструментальные средства информационных систем)/Laba6/db.php
@@ -1,14 +1,44 @@
 <?php
 
 //метод через rb.php:
-
-require 'rb-mysql.php';
-R::setup('mysql:host=localhost;dbname=komercheskaya_test4', 'root', 'root');
+// require 'libs/rb-mysql.php';
+// try {
     
+//     R::setup('mysql:host=127.0.0.1; port=3306;dbname=komercheskaya_test4', 'root', '');
+
+//     if (!R::testConnection()) {
+//         exit('<h1>Cannot connect to the database</h1>');
+//     } else {
+//         echo '<h1>You are connected.';
+//     }
+// } catch (Exception $e) {
+//     exit('<h1>Cannot connect to the database: </h1>' . $e->getMessage());
+// }
+
+// из методы:
+require 'libs/rb.php';
+
+R::setup('mysql:host=localhost; dbname=komercheskaya_test4', 'root', 'root');
+
+if (!R::testConnection()) {
+            exit('<h1>Cannot connect to the database</h1>');
+        }
+
+
+//метод через pdo:
+// require 'libs/rb.php';
+// try{
+//     $db = new PDO('mysql:host=localhost;dbname=komercheskaya_test4','root','');
+// } catch(PDOException $e){
+//     echo $e->getmessage();
+// }
+// print_r(PDO::getAvailableDrivers());
 
 //метод через composer:
 // require_once __DIR__ . '/vendor/autoload.php';
 // use \RedBeanPHP\R as R;
 // R::setup('mysql:host=localhost;dbname=komercheskaya_test4', 'root', 'root');
-
-?>
+// if (!R::testConnection()    )
+// {
+//     exit('нет подключения к базе данных');
+// }
